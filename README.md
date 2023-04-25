8dbe6b08b329117d697c154716f5addd41075e21
Tobenna <tvokafor@gmail.com>
2023-04-25 23:26:18 +0100

    Added a picture ,Read.me and refactored Javascript

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# week-3-
+# week-3-Passwordgenerator.
+
+Description
+
+This is a simple web-based application that generates a random password based on the user's specified criteria. It includes an HTML file (index.html), a CSS file (style.css), and a JavaScript file (script.js).
+Installation
+
+To run the code, simply download the files and open the index.html file in a web browser.
+Usage
+
+Upon opening the index.html file, the user will see a button labeled "Generate Password." When the user clicks the button, the application will prompt the user to specify the desired length of the password, as well as the inclusion of lowercase letters, uppercase letters, numbers, and special characters. The application will then generate a random password and display it in a text area labeled "Your Secure Password."
+Files
+index.html
+
+This file contains the structure of the web page, including the header, the main card element with the "Generate Password" button, and the text area for the generated password. It also includes links to the CSS and JavaScript files.
+style.css
+
+This file contains the styling for the web page, including the colors, fonts, and layout of the elements.
+script.js
+
+This file contains the JavaScript code for generating the random password. It includes a writePassword() function that prompts the user for password criteria and generates the password based on those criteria, as well as a generatePassword() function that generates the password itself.
+Credits
+
+This application was created by Tobenna.
+
+Links:
+![Alt text](week%203%20challenge%20password%20generator.png)
+http://localhost:52330/index.html
--- script.js
+++ script.js
@@ -1,54 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passwordText = document.querySelector("#password");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var passwordLength = "8-128 characters";
+  passwordText.value = password;
+}
+function generatePassword() {
+  var passwordLength = 0;
   var lowercaseLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-  passwordText.value = password;
+
+  passwordLength = window.prompt(
+    "How many characters would you like your password to contsin?"
+  );
   if (passwordLength === null) {
     return;
-  } else if (passwordLength < 8 || passwordLength > 128) {
-    alert("Please enter a password lenght between 8 and 128 characters.")
+  }
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a password lenght between 8 and 128 characters.");
     return;
   }
-  var includelowercaseLetters = check("Include lowercase letters");
-  var includeuppercaseLetters = check("Include uppercase letters");
-  var includenumbers = check("Include numbers");
-  var includespecialCharacters = check("Include special characters");
-  if (!includelowercaseLetters && !includeuppercaseLetters && !includenumbers && !includespecialCharacters) {
+  var includelowercaseLetters = window.confirm("Include lowercase letters");
+  var includeuppercaseLetters = window.confirm("Include uppercase letters");
+  var includenumbers = window.confirm("Include numbers");
+  var includespecialCharacters = window.confirm("Include special characters");
+  if (
+    !includelowercaseLetters &&
+    !includeuppercaseLetters &&
+    !includenumbers &&
+    !includespecialCharacters
+  ) {
     alert("Please select at least one character type.");
     return;
   }
-  var characterSet = "";
+  var characterSet = [];
   if (includelowercaseLetters) {
-    characterSet += lowercaseLetters;
+    characterSet = characterSet.concat(lowercaseLetters.split(""));
   }
   if (includeuppercaseLetters) {
-    characterSet += uppercaseLetters;
+    characterSet = characterSet.concat(uppercaseLetters.split(""));
   }
   if (includenumbers) {
-    characterSet += numbers;
+    characterSet = characterSet.concat(numbers.split(""));
   }
   if (includespecialCharacters) {
-    characterSet += specialCharacters
+    characterSet = characterSet.concat(specialCharacters.split(""));
   }
   var password = "";
-  for (var i = 0, i < passwordLength; i++) {
+  for (var i = 0; i < passwordLength; i++) {
     var randomindex = Math.floor(Math.random() * characterSet.length);
     password += characterSet[randomindex];
   }
   return password;
 }
 
-    
-
-
 // Add event listener to generate button
-var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
